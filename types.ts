bbe1a553229734ff766d98d571e36390ddc1c20b
Caio Cichetti <caio_cichetti@outlook.com>
2020-10-18 17:14:37 -0300

    Formatando com prettier

--- types.ts
+++ types.ts
@@ -5,7 +5,7 @@ isOpen = true;
 // string ('foo', "foo", `foo`)
 let message: string;
 message = 'foo';
-message = "foo";
+message = 'foo';
 message = `foo ${isOpen}`;
 
 // number (int, float, hex, binary)
@@ -14,30 +14,30 @@ total = 0xff0;
 
 // array (type[])
 let items: string[];
-items = ["foo", "bar"];
+items = ['foo', 'bar'];
 
 let values: Array<number>;
 values = [1, 2, 3];
 
 // tuple
 let title: [number, string, string];
-title = [1, "foo", "bar"];
+title = [1, 'foo', 'bar'];
 
 // enum
 enum Colors {
-  white = "#fff",
-  black = "#000",
+  white = '#fff',
+  black = '#000',
 }
 
 // any (qualquer coisa) NÃO É LEGAL!
 let coisa: any;
-coisa = "foo";
+coisa = 'foo';
 coisa = 2;
 coisa = [1];
 
 // void (vazio)
 function logger(): void {
-  console.log("foo");
+  console.log('foo');
 }
 
 // null | undefined
@@ -45,20 +45,20 @@ type Bla = string | undefined;
 
 // never
 function error(): never {
-  throw new Error("error");
+  throw new Error('error');
 }
 
 // object
 let cart: object;
 
 cart = {
-  key: "fi",
+  key: 'fi',
 };
 
 // Type Inference
-let message2 = "mensagem definida";
-message2 = "string nova";
+let message2 = 'mensagem definida';
+message2 = 'string nova';
 
-window.addEventListener("click", (e) => {
+window.addEventListener('click', (e) => {
   console.log(e.target);
 });
